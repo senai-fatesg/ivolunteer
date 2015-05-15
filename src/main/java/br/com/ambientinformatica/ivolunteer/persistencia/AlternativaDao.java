f29b7aceeb7eef28db6a30399d7c4abf16fa4a78
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:51:32 +0000

    Implementação de DAO Avliação e Alternativa

---
+++ src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AlternativaDao.java
@@ -0,0 +1,9 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Alternativa;
+import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+
+public interface AlternativaDao extends Persistencia<Alternativa> {
+	
+
+}

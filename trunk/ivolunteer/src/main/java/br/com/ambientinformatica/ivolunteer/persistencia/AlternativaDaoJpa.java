094e7f54c0839ca6af84701194220964376bde46
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:51:32 +0000

    Implementação de DAO Avliação e Alternativa

---
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AlternativaDaoJpa.java
@@ -0,0 +1,11 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Alternativa;
+import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
+
+public class AlternativaDaoJpa extends PersistenciaJpa<Alternativa> implements
+      AlternativaDao {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvaliacaoDao.java
@@ -0,0 +1,8 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Avaliacao;
+import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+
+public interface AvaliacaoDao extends Persistencia<Avaliacao> {
+
+}

f082e4ac943d23680ce17e0628a3ffc979675285
pvictor <pvictor@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:39:24 +0000

    Inclui as Anotações de Relação e @Temporal para as datas

--- src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Aluno.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Aluno.java
@@ -1,11 +1,16 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
 
 import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Aluno {
@@ -15,14 +20,19 @@ public class Aluno {
 	@SequenceGenerator(name = "aluno_seq", sequenceName = "aluno_seq", allocationSize = 1, initialValue = 1)
 	private Integer id;
 
+	@Temporal(TemporalType.DATE)
 	private Date dataSaida;
 
+	@Temporal(TemporalType.DATE)
 	private Date dataExame;
 
+	@OneToMany
 	private Frequencia frequencia;
 
+	@OneToMany
 	private RelatorioApredizagem relatorioApredizagem;
 
+	@OneToOne
 	private Matricula matricula;
 
 	public Date getDataSaida() {

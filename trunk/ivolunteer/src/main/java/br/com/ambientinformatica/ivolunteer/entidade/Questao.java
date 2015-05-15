52408c29b17180d1cc97caa7f30f9c665274bf18
brunosoares <brunosoares@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 01:27:04 +0000

    Adicionado o pacote ao qual a classe pertence.

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Questao.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Questao.java
@@ -1,22 +1,16 @@
+package br.com.ambientinformatica.ivolunteer.entidade;
+
 public class Questao {
 
 	private String pergunta;
 
 	private Avaliacao avaliacao;
 
-	private Avaliacao avaliacao;
-
 	private Objetiva objetiva;
 
 	private Discursivas discursivas;
 
 	private Questao questao;
-
-	private Questao questao;
-
-	private Objetiva objetiva;
-
-	private Discursivas discursivas;
 
 	private AvaliacaoQestao avaliacaoQestao;
 

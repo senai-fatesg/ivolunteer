6dd424ce8580a707161d184f2a5629b6a7f64ae2
gea.raphael <gea.raphael@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:47:54 +0000

    Foi corrigido os erros de importação que estavam dando. Add gets e sets. Add anotações do hibernate.

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Frequencia.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Frequencia.java
@@ -1,16 +1,43 @@
+package br.com.ambientinformatica.ivolunteer.entidade;
+
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Frequencia {
 
+	@Id
+	@GeneratedValue(generator = "frequencia_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "frequencia_seq", sequenceName = "frequencia_seq", allocationSize = 1, initialValue = 1)
+	private int id;
+
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date data;
 
-	private boolean presente;
+	private boolean presente = false;
 
-	private Funcionario funcionario;
+	public void setPresenca() {
+		this.presente =true;
+		this.data = new Date();
+	}
 
-	private Aluno aluno;
+	public int getId() {
+		return id;
+	}
 
-	private Aluno aluno;
+	public Date getData() {
+		return data;
+	}
 
-	public abstract void registraPresenca();
+	public boolean isPresente() {
+		return presente;
+	}
+
 }

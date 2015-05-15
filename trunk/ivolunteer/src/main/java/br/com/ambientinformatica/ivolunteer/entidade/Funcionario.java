63adbc2215faafffd23a0a6974dc03d7315f9dd2
vilmar <vilmar@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:37:07 +0000

    criado as anotações e gerado os getters and setters da classe Funcionario

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Funcionario.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Funcionario.java
@@ -1,8 +1,26 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Funcionario {
+
+	@Id
+	@GeneratedValue(generator = "funcionario_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "funcionario_seq", sequenceName = "funcionario_seq", allocationSize = 1, initialValue = 1)
+	private Integer id;
 
 	private String pis;
 
@@ -12,12 +30,15 @@ public class Funcionario {
 
 	private String reservista;
 
+	@Temporal(TemporalType.DATE)
 	private Date emissaoReservista;
 
 	private String cNH;
 
+	@Temporal(TemporalType.DATE)
 	private Date emissaoCNH;
 
+	@Temporal(TemporalType.DATE)
 	private Date validadeCNH;
 
 	private String nomePai;
@@ -34,12 +55,16 @@ public class Funcionario {
 
 	private boolean exameAdmissional;
 
+	@Temporal(TemporalType.DATE)
 	private Date dataExameAdmissional;
 
+	@Temporal(TemporalType.DATE)
 	private Date dataAdmissao;
 
+	@Temporal(TemporalType.DATE)
 	private Date inicioExperiencia;
 
+	@Temporal(TemporalType.DATE)
 	private Date terminoExperiencia;
 
 	private String banco;
@@ -48,6 +73,7 @@ public class Funcionario {
 
 	private String conta;
 
+	@Temporal(TemporalType.DATE)
 	private Date dataDemissao;
 
 	private Double valorAcerto;
@@ -56,16 +82,281 @@ public class Funcionario {
 
 	private String historico;
 
+	@Enumerated
 	private EnumFuncao funcao;
 
-	private Dependente[] dependente;
+	@OneToMany
+	private List<Dependente> dependente = new ArrayList<Dependente>();
 
-	private Frequencia frequencia;
+	@OneToMany
+	private List<Frequencia> frequencia = new ArrayList<Frequencia>();
 
-	private GradeHorario gradeHorario;
+	@OneToMany
+	private List<GradeHorario> gradeHorario = new ArrayList<GradeHorario>();
 
-	private AtividadeDiaria atividadeDiaria;
+	@OneToMany
+	private List<AtividadeDiaria> atividadeDiaria = new ArrayList<AtividadeDiaria>();
 
 	private Turma turma;
+
+	public String getPis() {
+		return pis;
+	}
+
+	public void setPis(String pis) {
+		this.pis = pis;
+	}
+
+	public String getCarteiraDeTrabalho() {
+		return carteiraDeTrabalho;
+	}
+
+	public void setCarteiraDeTrabalho(String carteiraDeTrabalho) {
+		this.carteiraDeTrabalho = carteiraDeTrabalho;
+	}
+
+	public String getSerieCarteiraDeTrabalho() {
+		return serieCarteiraDeTrabalho;
+	}
+
+	public void setSerieCarteiraDeTrabalho(String serieCarteiraDeTrabalho) {
+		this.serieCarteiraDeTrabalho = serieCarteiraDeTrabalho;
+	}
+
+	public String getReservista() {
+		return reservista;
+	}
+
+	public void setReservista(String reservista) {
+		this.reservista = reservista;
+	}
+
+	public Date getEmissaoReservista() {
+		return emissaoReservista;
+	}
+
+	public void setEmissaoReservista(Date emissaoReservista) {
+		this.emissaoReservista = emissaoReservista;
+	}
+
+	public String getcNH() {
+		return cNH;
+	}
+
+	public void setcNH(String cNH) {
+		this.cNH = cNH;
+	}
+
+	public Date getEmissaoCNH() {
+		return emissaoCNH;
+	}
+
+	public void setEmissaoCNH(Date emissaoCNH) {
+		this.emissaoCNH = emissaoCNH;
+	}
+
+	public Date getValidadeCNH() {
+		return validadeCNH;
+	}
+
+	public void setValidadeCNH(Date validadeCNH) {
+		this.validadeCNH = validadeCNH;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getZona() {
+		return zona;
+	}
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
+	public String getSecao() {
+		return secao;
+	}
+
+	public void setSecao(String secao) {
+		this.secao = secao;
+	}
+
+	public boolean isExameAdmissional() {
+		return exameAdmissional;
+	}
+
+	public void setExameAdmissional(boolean exameAdmissional) {
+		this.exameAdmissional = exameAdmissional;
+	}
+
+	public Date getDataExameAdmissional() {
+		return dataExameAdmissional;
+	}
+
+	public void setDataExameAdmissional(Date dataExameAdmissional) {
+		this.dataExameAdmissional = dataExameAdmissional;
+	}
+
+	public Date getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(Date dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public Date getInicioExperiencia() {
+		return inicioExperiencia;
+	}
+
+	public void setInicioExperiencia(Date inicioExperiencia) {
+		this.inicioExperiencia = inicioExperiencia;
+	}
+
+	public Date getTerminoExperiencia() {
+		return terminoExperiencia;
+	}
+
+	public void setTerminoExperiencia(Date terminoExperiencia) {
+		this.terminoExperiencia = terminoExperiencia;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getConta() {
+		return conta;
+	}
+
+	public void setConta(String conta) {
+		this.conta = conta;
+	}
+
+	public Date getDataDemissao() {
+		return dataDemissao;
+	}
+
+	public void setDataDemissao(Date dataDemissao) {
+		this.dataDemissao = dataDemissao;
+	}
+
+	public Double getValorAcerto() {
+		return valorAcerto;
+	}
+
+	public void setValorAcerto(Double valorAcerto) {
+		this.valorAcerto = valorAcerto;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public String getHistorico() {
+		return historico;
+	}
+
+	public void setHistorico(String historico) {
+		this.historico = historico;
+	}
+
+	public EnumFuncao getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(EnumFuncao funcao) {
+		this.funcao = funcao;
+	}
+
+	public List<Dependente> getDependente() {
+		return dependente;
+	}
+
+	public void setDependente(List<Dependente> dependente) {
+		this.dependente = dependente;
+	}
+
+	public List<Frequencia> getFrequencia() {
+		return frequencia;
+	}
+
+	public void setFrequencia(List<Frequencia> frequencia) {
+		this.frequencia = frequencia;
+	}
+
+	public List<GradeHorario> getGradeHorario() {
+		return gradeHorario;
+	}
+
+	public void setGradeHorario(List<GradeHorario> gradeHorario) {
+		this.gradeHorario = gradeHorario;
+	}
+
+	public List<AtividadeDiaria> getAtividadeDiaria() {
+		return atividadeDiaria;
+	}
+
+	public void setAtividadeDiaria(List<AtividadeDiaria> atividadeDiaria) {
+		this.atividadeDiaria = atividadeDiaria;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	public Integer getId() {
+		return id;
+	}
 
 }

02756572d6306d275112e51ddfeb9ce97a69017d
brunosoares <brunosoares@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:25:05 +0000

    Criação dos mapeamentos para JPA e também Gets e Sets das classes: Alternativa, Discursiva, Objetiva e Questao. *A classe "Discursiva" foi renomeada, antes estava como "Discursivas".

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Alternativa.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Alternativa.java
@@ -1,11 +1,57 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Alternativa {
 
+	@Id
+	@GeneratedValue(generator = "alternativa_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "alternativa_seq", sequenceName = "alternativa_seq", allocationSize = 1, initialValue = 1)
+	private Integer id;
+	
 	private String alternativa;
 
 	private boolean status;
 
 	private Objetiva objetiva;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAlternativa() {
+		return alternativa;
+	}
+
+	public void setAlternativa(String alternativa) {
+		this.alternativa = alternativa;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public Objetiva getObjetiva() {
+		return objetiva;
+	}
+
+	public void setObjetiva(Objetiva objetiva) {
+		this.objetiva = objetiva;
+	}
+	
+	
 
 }
--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Questao.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Questao.java
@@ -1,6 +1,18 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Questao {
+	
+	@Id
+	@GeneratedValue(generator = "questao_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "questao_seq", sequenceName = "questao_seq", allocationSize = 1, initialValue = 1)
+	private Integer id;
 
 	private String pergunta;
 
@@ -8,10 +20,68 @@ public class Questao {
 
 	private Objetiva objetiva;
 
-	private Discursivas discursivas;
+	private Discursiva discursivas;
 
 	private Questao questao;
 
 	private AvaliacaoQestao avaliacaoQestao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public Avaliacao getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(Avaliacao avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public Objetiva getObjetiva() {
+		return objetiva;
+	}
+
+	public void setObjetiva(Objetiva objetiva) {
+		this.objetiva = objetiva;
+	}
+
+	public Discursiva getDiscursivas() {
+		return discursivas;
+	}
+
+	public void setDiscursivas(Discursiva discursivas) {
+		this.discursivas = discursivas;
+	}
+
+	public Questao getQuestao() {
+		return questao;
+	}
+
+	public void setQuestao(Questao questao) {
+		this.questao = questao;
+	}
+
+	public AvaliacaoQestao getAvaliacaoQestao() {
+		return avaliacaoQestao;
+	}
+
+	public void setAvaliacaoQestao(AvaliacaoQestao avaliacaoQestao) {
+		this.avaliacaoQestao = avaliacaoQestao;
+	}
+	
+	
 
 }

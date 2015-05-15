2a3b78d3b4dfd3bfe77e5fa816442c7341c33120
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-15 22:14:27 +0000

    Correção da Classe Avaliação

--- src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Avaliacao.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Avaliacao.java
@@ -2,18 +2,83 @@ package br.com.ambientinformatica.ivolunteer.entidade;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Avaliacao {
+	
+	@Id
+	@GeneratedValue(generator="contato_seq", strategy=GenerationType.SEQUENCE)
+	@SequenceGenerator(name="contato_seq", sequenceName="contato_seq", allocationSize=1,initialValue=1)
+	private Integer id;
 
 	private String titulo;
 
 	private String descricao;
-
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date data;
-
+	
+	@OneToOne
 	private Pessoa pessoa;
 
-	private Questao questao;
-
+	@OneToOne
 	private AvaliacaoQestao avaliacaoQestao;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public AvaliacaoQestao getAvaliacaoQestao() {
+		return avaliacaoQestao;
+	}
+
+	public void setAvaliacaoQestao(AvaliacaoQestao avaliacaoQestao) {
+		this.avaliacaoQestao = avaliacaoQestao;
+	}
+	
+	
 
 }

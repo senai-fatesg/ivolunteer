509483ce34f0e52cb57501e3898d064af3d93d87
gea.raphael <gea.raphael@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 18:58:22 +0000

    Remossão da chamada de funcionario

--- trunk/ivolunteer/src/test/java/br/com/ambientinformatica/ivolunteer/test/GradeHorarioTest.java
+++ trunk/ivolunteer/src/test/java/br/com/ambientinformatica/ivolunteer/test/GradeHorarioTest.java
@@ -1,15 +1,9 @@
 package br.com.ambientinformatica.ivolunteer.test;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 import br.com.ambientinformatica.ivolunteer.entidade.EnumDiaSemana;
-import br.com.ambientinformatica.ivolunteer.entidade.Frequencia;
-import br.com.ambientinformatica.ivolunteer.entidade.Funcionario;
 import br.com.ambientinformatica.ivolunteer.entidade.GradeHorario;
-import br.com.ambientinformatica.ivolunteer.persistencia.FrequenciaDao;
-import br.com.ambientinformatica.ivolunteer.persistencia.FrequenciaDaoJpa;
 import br.com.ambientinformatica.ivolunteer.persistencia.GradeHorarioDao;
 import br.com.ambientinformatica.ivolunteer.persistencia.GradeHorarioDaoJpa;
 import br.com.ambientinformatica.jpa.exception.PersistenciaException;
@@ -20,12 +14,8 @@ public class GradeHorarioTest {
 	public void test() {
 		GradeHorario gradehorario = new GradeHorario();
 		EnumDiaSemana diaSemana = EnumDiaSemana.SEGUNDA;
-		Funcionario funcionario = new Funcionario();
-		funcionario.setAgencia("8405");
 
 		gradehorario.setDiaSemana(diaSemana);
-		gradehorario.setFuncionario(funcionario);
-
 		GradeHorarioDao gradehorarioDao = new GradeHorarioDaoJpa();
 
 		try {

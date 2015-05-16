5016f02e21f8604aff3e10b9af2c327b88033745
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 11:32:34 +0000

    Criação da Questão Dao

---
+++ src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/QuestaoDao.java
@@ -0,0 +1,8 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Questao;
+import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+
+public interface QuestaoDao extends Persistencia<Questao> {
+	
+}

cea848ba9090ffc349cc271a7bea907327c19be4
pvictor <pvictor@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 17:09:27 +0000

    Implemetação dos Atributos e Métodos

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/EnumTurno.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/EnumTurno.java
@@ -1,3 +1,18 @@
-public class EnumTurno {
+package br.com.ambientinformatica.ivolunteer.entidade;
 
+import br.com.ambientinformatica.util.IEnum;
+
+public enum EnumTurno implements IEnum {
+
+	Matutino("Matutino"), Vespertino("Verpertino"), Noturno("Noturno");
+
+	private String descricao;
+
+	private EnumTurno(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }

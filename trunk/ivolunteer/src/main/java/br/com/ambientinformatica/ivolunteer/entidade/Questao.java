894062f0a7f5d3f4e1f046bd0a5836cdf9990341
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 12:01:04 +0000

    Implementação do Equals e HashCode para execução metodo add

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Questao.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Questao.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
@@ -18,6 +17,8 @@ public class Questao {
 	@GeneratedValue(generator = "questao_seq", strategy = GenerationType.SEQUENCE)
 	@SequenceGenerator(name = "questao_seq", sequenceName = "questao_seq", allocationSize = 1, initialValue = 1)
 	private Integer id;
+	
+	private int ordem;
 
 	private String pergunta;	
 
@@ -63,8 +64,35 @@ public class Questao {
 		this.discursiva = discursiva;
 	}
 
-	
-	
+	public int getOrdem() {
+		return ordem;
+	}
+
+	public void setOrdem(int ordem) {
+		this.ordem = ordem;
+	}
+
+	@Override
+   public int hashCode() {
+	   final int prime = 31;
+	   int result = 1;
+	   result = prime * result + ordem;
+	   return result;
+   }
+
+	@Override
+   public boolean equals(Object obj) {
+	   if (this == obj)
+		   return true;
+	   if (obj == null)
+		   return false;
+	   if (getClass() != obj.getClass())
+		   return false;
+	   Questao other = (Questao) obj;
+	   if (ordem != other.ordem)
+		   return false;
+	   return true;
+   }
 	
 
 }

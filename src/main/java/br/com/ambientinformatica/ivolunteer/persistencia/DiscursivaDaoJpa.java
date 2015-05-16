399e6ce675c3d2216eefa853c1fb9038bce7ece7
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 00:30:34 +0000

    Criação da DAO para Objetiva e Discursiva.

---
+++ src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/DiscursivaDaoJpa.java
@@ -0,0 +1,11 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Discursiva;
+import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
+
+public class DiscursivaDaoJpa extends PersistenciaJpa<Discursiva> implements
+      DiscursivaDao {
+
+	private static final long serialVersionUID = 1L;
+
+}

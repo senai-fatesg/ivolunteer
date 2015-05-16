92281d8e792afb6eb7ff9015966e8e2553b111ee
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 00:45:07 +0000

    Alternativa Test

---
+++ trunk/ivolunteer/src/test/java/ivolunteer/AlternativaTest.java
@@ -0,0 +1,14 @@
+package ivolunteer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlternativaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

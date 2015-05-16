15da8130eea17d8451514b9e687fb9295aff7f5f
gea.raphael <gea.raphael@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 02:35:37 +0000

    Criação das classes de persistencia, e da classe de teste.

---
+++ src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/FrequenciaDao.java
@@ -0,0 +1,8 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Frequencia;
+import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+
+public interface FrequenciaDao extends Persistencia<Frequencia> {
+
+}

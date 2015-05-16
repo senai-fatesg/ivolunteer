cfae4e673ae4e14537fe9256dc1c3aab91ae9c3e
andrevictor <andrevictor@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 13:07:19 +0000

    teste inciado, em fase de desenvolvimento.

--- src/test/java/br/com/ambientinformatica/ivolunteer/test/GradeHorarioTest.java
+++ src/test/java/br/com/ambientinformatica/ivolunteer/test/GradeHorarioTest.java
@@ -4,12 +4,35 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import br.com.ambientinformatica.ivolunteer.entidade.EnumDiaSemana;
+import br.com.ambientinformatica.ivolunteer.entidade.Frequencia;
+import br.com.ambientinformatica.ivolunteer.entidade.Funcionario;
+import br.com.ambientinformatica.ivolunteer.entidade.GradeHorario;
+import br.com.ambientinformatica.ivolunteer.persistencia.FrequenciaDao;
+import br.com.ambientinformatica.ivolunteer.persistencia.FrequenciaDaoJpa;
+import br.com.ambientinformatica.ivolunteer.persistencia.GradeHorarioDao;
+import br.com.ambientinformatica.ivolunteer.persistencia.GradeHorarioDaoJpa;
+import br.com.ambientinformatica.jpa.exception.PersistenciaException;
+
 public class GradeHorarioTest {
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void test() {
-		fail("Not yet implemented");
-	}
+		GradeHorario gradehorario = new GradeHorario();
+		EnumDiaSemana diaSemana = EnumDiaSemana.SEGUNDA;
+		Funcionario funcionario = new Funcionario();
+		funcionario.setAgencia("8405");
 
-	
+		gradehorario.setDiaSemana(diaSemana);
+		gradehorario.setFuncionario(funcionario);
+
+		GradeHorarioDao gradehorarioDao = new GradeHorarioDaoJpa();
+
+		try {
+			gradehorarioDao.incluir(gradehorario);
+		} catch (PersistenciaException e) {
+			e.printStackTrace();
+
+		}
+	}
 }

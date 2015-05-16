ecb0bd295e9425f280770c05f910373be6b2e731
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 11:21:19 +0000

    Correções na Avalização Dao

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvaliacaoDao.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvaliacaoDao.java
@@ -4,5 +4,5 @@ import br.com.ambientinformatica.ivolunteer.entidade.Avaliacao;
 import br.com.ambientinformatica.jpa.persistencia.Persistencia;
 
 public interface AvaliacaoDao extends Persistencia<Avaliacao> {
-
+	
 }

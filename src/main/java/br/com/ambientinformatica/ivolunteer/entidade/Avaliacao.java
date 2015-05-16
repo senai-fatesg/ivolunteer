c557579896153ab4e761fba6a3c65014342e999a
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 11:27:28 +0000

    Correção de Mapeamento

--- src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Avaliacao.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Avaliacao.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
@@ -30,7 +30,7 @@ public class Avaliacao {
 	@OneToOne
 	private Pessoa pessoa;
 
-	@ManyToOne
+	@OneToMany
 	private List<AvaliacaoQestao> avaliacaoQestao;
 
 	public Integer getId() {

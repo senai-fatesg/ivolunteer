d8a86182d85c38f67c4d330d09d9e96dd534fe71
andrevictor <andrevictor@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 00:52:59 +0000

    classe de teste Junit

---
+++ trunk/ivolunteer/src/test/java/ivolunteer/GradeHorarioTest.java
@@ -0,0 +1,15 @@
+package ivolunteer;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class GradeHorarioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+}

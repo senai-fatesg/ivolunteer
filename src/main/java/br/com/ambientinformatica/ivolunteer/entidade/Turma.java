7b8c598f7ab54f2be4d2405836dce963ec6b0ff6
pvictor <pvictor@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 17:46:19 +0000

    Revisado e Ajustado de Acordo com o diagrama de Classes.

--- src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Turma.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Turma.java
@@ -1,36 +1,37 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
+
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Turma {
 
 	@Id
-	@GeneratedValue(generator="turma_seq", strategy=GenerationType.SEQUENCE)
-	@SequenceGenerator(name="turma_seq", sequenceName="turma_seq", allocationSize=1, initialValue= 1)
+	@GeneratedValue(generator = "turma_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "turma_seq", sequenceName = "turma_seq", allocationSize = 1, initialValue = 1)
 	private Integer id;
-	
+
 	private int maximoAlunos;
-	
+
 	private EnumTurno turno;
-	
+
 	private String nome;
+
 	@ManyToOne
 	private AgrupamentoTurma agrupamentoTurma;
-	
+
 	@OneToMany
 	private List<Funcionario> funcionario;
-	
-	@OneToOne
+
 	private Matricula matricula;
+
+	private RelatorioAprendizagem relatorioAprendizagem;
 
 	public Integer getId() {
 		return id;
@@ -87,7 +88,14 @@ public class Turma {
 	public void setMatricula(Matricula matricula) {
 		this.matricula = matricula;
 	}
-	
-	
+
+	public RelatorioAprendizagem getRelatorioAprendizagem() {
+		return relatorioAprendizagem;
+	}
+
+	public void setRelatorioAprendizagem(
+			RelatorioAprendizagem relatorioAprendizagem) {
+		this.relatorioAprendizagem = relatorioAprendizagem;
+	}
 
 }

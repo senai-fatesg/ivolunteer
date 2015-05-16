3c51394bb448885b442a291d2b9428357c470832
vilmar <vilmar@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 01:08:49 +0000

    Criado as classes de persistencia FuncionarioDao, FuncionarioDaoJpa e FuncionarioTest

---
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/FuncionarioDao.java
@@ -0,0 +1,9 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Funcionario;
+import br.com.ambientinformatica.jpa.persistencia.Persistencia;
+
+
+public interface  FuncionarioDao extends Persistencia<Funcionario> {
+
+}
---
+++ trunk/ivolunteer/src/test/java/ivolunteer/FuncionarioTest.java
@@ -0,0 +1,14 @@
+package ivolunteer;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class FuncionarioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

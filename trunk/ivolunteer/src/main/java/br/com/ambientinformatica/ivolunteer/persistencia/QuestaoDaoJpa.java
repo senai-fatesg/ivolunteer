c2a66341e8fe6e511e4fc232ade6e10d5e5141b5
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 11:32:34 +0000

    Criação da Questão Dao

---
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/QuestaoDaoJpa.java
@@ -0,0 +1,10 @@
+package br.com.ambientinformatica.ivolunteer.persistencia;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Questao;
+import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
+
+public class QuestaoDaoJpa extends PersistenciaJpa<Questao> implements
+      QuestaoDao {
+	private static final long serialVersionUID = 1L;
+
+}

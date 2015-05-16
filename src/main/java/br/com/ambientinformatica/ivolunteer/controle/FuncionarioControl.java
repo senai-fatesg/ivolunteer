0d0718d79bf5a027af2898b6140de71e3bc62897
vilmar <vilmar@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-16 18:59:01 +0000

    Criado camada de controler da classe Funcionario

---
+++ src/main/java/br/com/ambientinformatica/ivolunteer/controle/FuncionarioControl.java
@@ -0,0 +1,56 @@
+package br.com.ambientinformatica.ivolunteer.controle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.event.ActionEvent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
+import br.com.ambientinformatica.ivolunteer.entidade.Funcionario;
+import br.com.ambientinformatica.ivolunteer.persistencia.FuncionarioDao;
+
+@Controller("ContatoControl")
+@Scope("conversation")
+public class FuncionarioControl {
+
+	Funcionario funcionario = new Funcionario();
+
+	@Autowired
+	private FuncionarioDao funcionarioDao;
+
+	private List<Funcionario> funcionarios = new ArrayList<Funcionario>();
+
+	public void confirmar(ActionEvent evt) {
+		try {
+			funcionarioDao.alterar(funcionario);
+			funcionario = new Funcionario();
+		} catch (Exception e) {
+			UtilFaces.addMensagemFaces(e);
+		}
+	}
+
+	public void listar(ActionEvent evt) {
+		try {
+			funcionarios = funcionarioDao.listar();
+		} catch (Exception e) {
+			UtilFaces.addMensagemFaces(e);
+		}
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+}

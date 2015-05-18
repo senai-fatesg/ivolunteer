36aa2b6ee9af1811ce3f1d96a83294229353ba09
edjalma <edjalma@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-18 02:00:51 +0000

    Criado pacote para testes de Interface com Selenium e JUnit. Criado pacote para testes Funcional com o Arquillian. Criado arquivos de configuracao para o Arquillian funcionar. Alterado o pom.xml para adicionar bibliotecas necessarias.

---
+++ src/test/java/br/com/ambientinformatica/ivolunteer/test/junitselenium/LoginTestIT.java
@@ -0,0 +1,106 @@
+package br.com.ambientinformatica.ivolunteer.test.junitselenium;
+
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * 
+ * Classe de exemplo de como construir um caso de teste baseado em interface
+ * 
+ * Teste feito utilizando framework Selenium
+ * 
+ * Para criar seus proprios teste utilizar Selenium - Web Browser (Firefox)
+ * {@link http://www.seleniumhq.org/}
+ * 
+ * @author edjalma
+ *
+ */
+public class LoginTestIT {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  /**
+   * Metodo inicial do Teste para ser executado via JUnit
+   * @throws Exception
+   */
+  @Test
+  public void testExemploIVolunteer() throws Exception {
+    driver.get(baseUrl + "/ivolunteer/");
+    driver.findElement(By.xpath("//input[@value='Entrar']")).click();
+    //clicar no menu de Contato (Caso exista)
+    driver.findElement(By.linkText("Contato")).click();
+    //Limpa o campo 
+    driver.findElement(By.id("formCorpo:consEmpresa_input")).clear();
+    //insere um valor no input
+    driver.findElement(By.id("formCorpo:consEmpresa_input")).sendKeys("Empresa a");
+    
+    driver.findElement(By.id("formCorpo:nome")).clear();
+    driver.findElement(By.id("formCorpo:nome")).sendKeys("Ambient");
+    driver.findElement(By.id("formCorpo:email")).clear();
+    driver.findElement(By.id("formCorpo:email")).sendKeys("aureaqs@gmail.com");
+    driver.findElement(By.id("formCorpo:j_idt29")).click();
+    driver.findElement(By.xpath("//input[@value='Sair']")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}

9ffcbb2fb8938cf993a1af6f8b66272b167fcba9
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-23 19:16:52 +0000

    Publicação para Ajuda dos caras

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/AvaliacaoControl.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/AvaliacaoControl.java
@@ -3,8 +3,10 @@ package br.com.ambientinformatica.ivolunteer.controle;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.event.ActionEvent;
 
+import org.primefaces.event.SelectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -18,9 +20,8 @@ import br.com.ambientinformatica.ivolunteer.entidade.Objetiva;
 import br.com.ambientinformatica.ivolunteer.entidade.Questao;
 import br.com.ambientinformatica.ivolunteer.persistencia.AvaliacaoDao;
 
-
 @Controller("AvaliacaoControl")
-//@ManagedBean
+// @ManagedBean
 @Scope("conversation")
 public class AvaliacaoControl {
 
@@ -28,38 +29,30 @@ public class AvaliacaoControl {
 	private Questao questao = new Questao();
 	private Discursiva discursiva = new Discursiva();
 	private Objetiva objetiva = new Objetiva();
-	private Alternativa alternativa =  new Alternativa();
-	private boolean mostraObjetiva,mostraDiscursiva = false;
-	
- 	
-	public boolean isMostraDiscursiva() {
-		return mostraDiscursiva;
-	}
-
-
-
-	public void setMostraDiscursiva(boolean mostraDiscursiva) {
-		this.mostraDiscursiva = mostraDiscursiva;
-	}
-
-
-
-	public boolean isMostraObjetiva() {
-		return mostraObjetiva;
-	}
-
-
-
-	public void setMostraObjetiva(boolean mostraObjetiva) {
-		this.mostraObjetiva = mostraObjetiva;
-	}
-
-	private EnumQuestao tipoQuestaoSelecionada;	
+	private Alternativa alternativa = new Alternativa();
+	private Boolean mostraObjetiva, mostraDiscursiva = new Boolean(true);
+	private EnumQuestao tipoQuestaoSelecionada;
 
 	@Autowired
 	private AvaliacaoDao avaliacaoDao;
-	
-	public void adicionarQuestaoDiscursiva(ActionEvent evt){
+
+	public Boolean getMostraObjetiva() {
+		return mostraObjetiva;
+	}
+
+	public void setMostraObjetiva(Boolean mostraObjetiva) {
+		this.mostraObjetiva = mostraObjetiva;
+	}
+
+	public Boolean getMostraDiscursiva() {
+		return mostraDiscursiva;
+	}
+
+	public void setMostraDiscursiva(Boolean mostraDiscursiva) {
+		this.mostraDiscursiva = mostraDiscursiva;
+	}
+
+	public void adicionarQuestaoDiscursiva(ActionEvent evt) {
 		try {
 			this.questao.setDiscursiva(this.discursiva);
 			this.avaliacao.addQuestao(this.questao);
@@ -69,40 +62,38 @@ public class AvaliacaoControl {
 		}
 	}
 	
-	
-
+	public void adicionarAlternativaQuestao(ActionEvent evt){
+		try {
+			this.objetiva.addAlternativa(this.alternativa);
+			this.alternativa = new Alternativa();
+		} catch (Exception e) {
+			UtilFaces.addMensagemFaces(e);
+		}		
+	}
 	public Objetiva getObjetiva() {
 		return objetiva;
 	}
-
-
 
 	public void setObjetiva(Objetiva objetiva) {
 		this.objetiva = objetiva;
 	}
 
-
-
 	public Alternativa getAlternativa() {
 		return alternativa;
 	}
-
-
 
 	public void setAlternativa(Alternativa alternativa) {
 		this.alternativa = alternativa;
 	}
 
-
-
 	public Avaliacao getAvaliacao() {
-		return this.avaliacao;		
+		return this.avaliacao;
 	}
 
 	public void setAvaliacao(Avaliacao avaliacao) {
 		this.avaliacao = avaliacao;
-		
-	}	
+
+	}
 
 	public void confirmar(ActionEvent evt) {
 		try {
@@ -115,12 +106,12 @@ public class AvaliacaoControl {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
-	public List<String> completeEnumTipoQuestao(String query){
+
+	public List<String> completeEnumTipoQuestao(String query) {
 		List<String> retorno = new ArrayList<String>();
 		EnumQuestao[] enunsQuestao = EnumQuestao.values();
 		for (int i = 0; i < enunsQuestao.length; i++) {
-			retorno.add(enunsQuestao[i].getDescricao());			
+			retorno.add(enunsQuestao[i].getDescricao());
 		}
 		return retorno;
 	}
@@ -147,8 +138,7 @@ public class AvaliacaoControl {
 
 	public void setDiscursiva(Discursiva discursiva) {
 		this.discursiva = discursiva;
-				
-	}	
 
-	
+	}
+
 }

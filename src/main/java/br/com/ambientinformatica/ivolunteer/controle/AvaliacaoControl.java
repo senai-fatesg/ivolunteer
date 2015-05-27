28ad7f830c03c9e3e1bc7d679539b0f25f42688b
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-27 00:43:11 +0000

    Control

--- src/main/java/br/com/ambientinformatica/ivolunteer/controle/AvaliacaoControl.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/controle/AvaliacaoControl.java
@@ -1,10 +1,9 @@
 package br.com.ambientinformatica.ivolunteer.controle;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.event.ActionEvent;
 
+import org.primefaces.component.tabview.Tab;
+import org.primefaces.event.TabChangeEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -13,52 +12,51 @@ import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
 import br.com.ambientinformatica.ivolunteer.entidade.Alternativa;
 import br.com.ambientinformatica.ivolunteer.entidade.Avaliacao;
 import br.com.ambientinformatica.ivolunteer.entidade.Discursiva;
-import br.com.ambientinformatica.ivolunteer.entidade.EnumQuestao;
 import br.com.ambientinformatica.ivolunteer.entidade.Objetiva;
 import br.com.ambientinformatica.ivolunteer.entidade.Questao;
+import br.com.ambientinformatica.ivolunteer.entidade.EnumQuestao;
 import br.com.ambientinformatica.ivolunteer.persistencia.AvaliacaoDao;
+
 
 @Controller("AvaliacaoControl")
 @Scope("conversation")
 public class AvaliacaoControl {
 
-	private Avaliacao avaliacao = new Avaliacao();
-	private Questao questao = new Questao();
+	private Avaliacao avaliacao = new Avaliacao();	
 	private Discursiva discursiva = new Discursiva();
-	private List<Alternativa> alternativas = new ArrayList<Alternativa>();
+	private Questao questao = new Questao();
 	private Objetiva objetiva = new Objetiva();
-	private Alternativa alternativa = new Alternativa();	
+	EnumQuestao tipoQuestao = EnumQuestao.D;
+	private Alternativa alternativa = new Alternativa();
 
 	@Autowired
-	private AvaliacaoDao avaliacaoDao;	
+	private AvaliacaoDao avaliacaoDao;
 
-	public void adicionarQuestaoDiscursiva(ActionEvent evt) {
+	//Insere Alternativas em Questao do tipo Objetiva 
+	public void addAlternativa(ActionEvent ev) {
 		try {
-			this.questao.setDiscursiva(this.discursiva);
-			this.avaliacao.addQuestao(this.questao);
-			this.questao = null;
-		} catch (Exception e) {
-			UtilFaces.addMensagemFaces(e);
-		}
-	}
-
-	public void addAlternativaQuestao(ActionEvent ev) {
-		try {
-			this.objetiva.addAlternativa(alternativa);
+			this.objetiva.addAlternativa(alternativa);			
 			this.alternativa = new Alternativa();
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e);
 		}
-	}
+	}	
+	//Insere Questao em Avaliacao
 	
-	public void removerAlterntaivaQuetao(Alternativa alternativa){
+
+	public void removerAlterntaivaQuetao(Alternativa alternativa) {
 		try {
 			this.objetiva.removerAlternativa(alternativa);
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-
+	
+	public void alteraTab(TabChangeEvent event){
+		Tab tabAtual = event.getTab();
+		tabAtual.get
+	}
+	
 	public Objetiva getObjetiva() {
 		return objetiva;
 	}
@@ -82,14 +80,15 @@ public class AvaliacaoControl {
 	public void setAvaliacao(Avaliacao avaliacao) {
 		this.avaliacao = avaliacao;
 
-	}	
+	}
+
 	public Questao getQuestao() {
 		return questao;
 	}
 
 	public void setQuestao(Questao questao) {
 		this.questao = questao;
-	}	
+	}
 
 	public Discursiva getDiscursiva() {
 		return discursiva;

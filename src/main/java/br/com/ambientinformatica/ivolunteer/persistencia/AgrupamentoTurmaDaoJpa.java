ceadc318b20ff3fd09e1a78021ec04cde5b20eea
ricardo0057 <ricardo0057@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-29 00:03:30 +0000

    Agrupamento Turma jpa

--- src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AgrupamentoTurmaDaoJpa.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AgrupamentoTurmaDaoJpa.java
@@ -1,5 +1,9 @@
 package br.com.ambientinformatica.ivolunteer.persistencia;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.ambientinformatica.ivolunteer.entidade.AgrupamentoTurma;
@@ -10,5 +14,14 @@ public class AgrupamentoTurmaDaoJpa extends PersistenciaJpa<AgrupamentoTurma>
 		implements AgrupamentoTurmaDao {
 
 	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings("unchecked")
+	public List<AgrupamentoTurma> listaPerNome(String nome) {
+		Query query = em.createQuery("select a from "
+				+ "AgrupamentoTurma a where upper(a.identificador) like :nome");
+		query.setParameter("nome", "%" + nome.toUpperCase() + "%");
+
+		return (List<AgrupamentoTurma>) query.getResultList();
+	}
 
 }

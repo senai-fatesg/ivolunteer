1385b4c3d46f03275bdb8397343cd66037a26af7
ricardo0057 <ricardo0057@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-05-30 00:21:34 +0000

    turma control

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/TurmaControl.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/TurmaControl.java
@@ -10,11 +10,8 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
-import br.com.ambientinformatica.ivolunteer.entidade.AgrupamentoTurma;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumTurno;
 import br.com.ambientinformatica.ivolunteer.entidade.Turma;
-import br.com.ambientinformatica.ivolunteer.persistencia.AgrupamentoTurmaDao;
-import br.com.ambientinformatica.ivolunteer.persistencia.AgrupamentoTurmaDaoJpa;
 import br.com.ambientinformatica.ivolunteer.persistencia.TurmaDao;
 
 @Controller("TurmaControl")
@@ -25,7 +22,8 @@ private Turma turma = new Turma();
 	
 	@Autowired
 	private TurmaDao turmaDao;
-	
+
+	private EnumTurno turnoselecionado;
 	private List<Turma> turmas = new ArrayList<Turma>();
 	
 
@@ -70,13 +68,6 @@ private Turma turma = new Turma();
 		this.turmas = turmas;
 	}
 
-
-	
-
-	private EnumTurno turnoselecionado;
-	private AgrupamentoTurma agrupamentoTurma = new AgrupamentoTurma();
-	private AgrupamentoTurmaDao agrupamentoTurmaDao = new AgrupamentoTurmaDaoJpa();
-
 	public EnumTurno getTurnoselecionado() {
 		return turnoselecionado;
 	}
@@ -84,22 +75,6 @@ private Turma turma = new Turma();
 	public void setTurnoselecionado(EnumTurno turnoselecionado) {
 		this.turnoselecionado = turnoselecionado;
 	}
-	
-	
-	public AgrupamentoTurma getAgrupamentoTurma() {
-		return agrupamentoTurma;
-	}
-
-	public void setAgrupamentoTurmma(AgrupamentoTurma agrupamentoTurma) {
-		this.agrupamentoTurma = agrupamentoTurma;
-	}
-
-	public List<AgrupamentoTurma> consultarAgrupamento(String nome){
-	
-		List<AgrupamentoTurma> agrupamento = agrupamentoTurmaDao.listaPerNome(nome);
-		return agrupamento;
-	}
-	
 	
 	public List<String> completeEnumTurno(String query){
 		List<String> retorno = new ArrayList<String>();

b34efa0bc2df0f7c6e2eb6809e32c2be10fee166
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-06-06 00:31:46 +0000

    Listar Pronto em Tela

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvaliacaoDao.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvaliacaoDao.java
@@ -1,8 +1,10 @@
 package br.com.ambientinformatica.ivolunteer.persistencia;
 
+import java.util.List;
+
 import br.com.ambientinformatica.ivolunteer.entidade.Avaliacao;
 import br.com.ambientinformatica.jpa.persistencia.Persistencia;
 
 public interface AvaliacaoDao extends Persistencia<Avaliacao> {
-	
+	public List<Avaliacao> listarTitulo(Avaliacao avaliacao);
 }
--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvalicaoDaoJpa.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/AvalicaoDaoJpa.java
@@ -1,11 +1,29 @@
 package br.com.ambientinformatica.ivolunteer.persistencia;
 
+
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.ambientinformatica.ivolunteer.entidade.Avaliacao;
 import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 
 @Repository("avaliacaoDao")
-public class AvalicaoDaoJpa extends PersistenciaJpa<Avaliacao> implements AvaliacaoDao{
+public class AvalicaoDaoJpa extends PersistenciaJpa<Avaliacao> implements
+		AvaliacaoDao {
 	private static final long serialVersionUID = 1L;
+	
+	public List<Avaliacao> listarTitulo(Avaliacao avaliacao) {
+		try {
+			Query  q =  em.createQuery("select a from Avaliacao a where a.titulo like :titulo");
+			q.setParameter("titulo", "%"+avaliacao.getTitulo()+"%");
+			return q.getResultList();
+		} catch (EntityNotFoundException e) {
+			return null;
+		} 	
+	}
 }

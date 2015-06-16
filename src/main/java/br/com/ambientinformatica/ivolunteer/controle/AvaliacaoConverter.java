51c3c11d45d46fb87a41009a2e5bf09121070acc
luiz.alfredo <luiz.alfredo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-06-16 23:39:33 +0000

    Commit para implementação de tela

---
+++ src/main/java/br/com/ambientinformatica/ivolunteer/controle/AvaliacaoConverter.java
@@ -0,0 +1,50 @@
+package br.com.ambientinformatica.ivolunteer.controle;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.com.ambientinformatica.ivolunteer.entidade.Avaliacao;
+import br.com.ambientinformatica.ivolunteer.entidade.Funcionario;
+import br.com.ambientinformatica.ivolunteer.persistencia.AvaliacaoDao;
+import br.com.ambientinformatica.jpa.util.FabricaAbstrata;
+
+@FacesConverter("avaliacaoConverter")
+public class AvaliacaoConverter implements Converter {
+
+	private AvaliacaoDao avaliacaoDao = (AvaliacaoDao) FabricaAbstrata
+			.criarObjeto("avaliacaoDao");
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		if (value != null && !value.trim().equals("")) {
+			Avaliacao avaliacao = new Avaliacao();
+			try {
+				int id = Integer.parseInt(value);
+				try {
+					avaliacao = avaliacaoDao.consultar(id);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} catch (NumberFormatException exception) {
+				return null;
+			}
+			return avaliacao;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		if (value == null || value.equals("")) {
+			return "";
+		} else {
+			return String.valueOf(((Avaliacao) value).getId());
+		}
+	}
+
+}

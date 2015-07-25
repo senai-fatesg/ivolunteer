72b2bdeb2bf600c92bef4aedacfbdcdbf997d997
aureliano <aureliano@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2015-07-25 16:26:51 +0000

    correção da tela de pesquisa e seleção de candidato

--- src/main/java/br/com/ambientinformatica/ivolunteer/controle/PesquisaSelecaoControl.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/controle/PesquisaSelecaoControl.java
@@ -15,6 +15,7 @@ import br.com.ambientinformatica.ivolunteer.entidade.EnumPrioridade;
 import br.com.ambientinformatica.ivolunteer.entidade.Pessoa;
 import br.com.ambientinformatica.ivolunteer.entidade.SelecaoCandidato;
 import br.com.ambientinformatica.ivolunteer.persistencia.PessoaDao;
+import br.com.ambientinformatica.jpa.exception.PersistenciaException;
 
 @Controller("PesquisaSelecaoControl")
 @Scope("conversation")
@@ -23,11 +24,16 @@ public class PesquisaSelecaoControl {
 	private Pessoa pessoa = new Pessoa();
 
 	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
-	
-	private SelecaoCandidato selecaoCandidato = new SelecaoCandidato();	
+
+	// instancia de candidato carregada para realização de consulta
+	private Pessoa candidatoConsulta = new Pessoa();
+	// lista utilizada na consulta do candidato
+	private List<Pessoa> listaCandidato = new ArrayList<Pessoa>();
+
+	private SelecaoCandidato selecaoCandidato = new SelecaoCandidato();
 
 	@Autowired
-	private PessoaDao PessoaDao;
+	private PessoaDao pessoaDao;
 
 	public void setPessoas(List<Pessoa> pessoas) {
 		this.pessoas = pessoas;
@@ -36,44 +42,78 @@ public class PesquisaSelecaoControl {
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
-	
+
 	public void confirmar(ActionEvent evt) {
 		try {
-			PessoaDao.alterar(pessoa);
-			listar(evt);
+			pessoaDao.alterar(candidatoConsulta);
+			//listar(evt);
+			candidatoConsulta = new Pessoa();
+			UtilFaces.addMensagemFaces("Informações salvas com sucesso!");
+			candidatoConsulta = new Pessoa();
 			pessoa = new Pessoa();
 		} catch (Exception e) {
-			UtilFaces.addMensagemFaces(e);
+			UtilFaces.addMensagemFaces("Ocorreu uma falha ao tentar gravar as informações de candidato");
 		}
 	}
 
 	public void consultar(ActionEvent evt) {
 		try {
-			PessoaDao.consultar(pessoa.getId());
+			candidatoConsulta = pessoaDao.consultar(pessoa.getId());
 			pessoa = new Pessoa();
 		} catch (Exception e) {
-			UtilFaces.addMensagemFaces(e);
+			UtilFaces.addMensagemFaces("Ocorreu uma falha ao tentar consultar o candidato.");
 		}
+	}
+
+	public void carregaPessoaAlteracao(Pessoa pessoa)
+	      throws PersistenciaException {
+		this.candidatoConsulta = pessoaDao.consultarPessoaCompleta(pessoa);		
 	}
 
 	public void descricaoDialogo(ActionEvent evt) {
 		try {
-			PessoaDao.alterar(pessoa);
-			pessoas = PessoaDao.listar();
+			pessoaDao.alterar(pessoa);
+			pessoas = pessoaDao.listar();
 		} catch (Exception erro) {
 			UtilFaces.addMensagemFaces(erro);
 		}
 	}
 
+	public List<Pessoa> getListaCandidato() {
+		return listaCandidato;
+	}
+
+	public void setListaCandidato(List<Pessoa> listaCandidato) {
+		this.listaCandidato = listaCandidato;
+	}
+
 	public void listar(ActionEvent evt) {
 		try {
-			pessoas = PessoaDao.pesquisaSelecaoCandidato(selecaoCandidato);
+			pessoas = pessoaDao.pesquisaSelecaoCandidato(selecaoCandidato);
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e.getMessage());
 		}
 	}
 
-	
+	// Aplica Filtro
+	public void aplicarFiltro(ActionEvent evt) {
+		try {
+			// listaCandidato =
+			// pessoaDao.pesquisaSelecaoCandidato(selecaoCandidato);
+
+			if (candidatoConsulta.getNomePessoa() == null
+			      || candidatoConsulta.getNomePessoa().isEmpty()) {
+				listaCandidato = pessoaDao.listaCandidato();
+			} else {
+				listaCandidato = pessoaDao.listaCandidatoPorNome(this.pessoa
+				      .getNomePessoa());
+			}
+		} catch (Exception e) {
+			UtilFaces.addMensagemFaces(e);
+		}
+
+	}
+
 	public List<SelectItem> getCompleteEnumPrioridade() {
 		return UtilFaces.getListEnum(EnumPrioridade.values());
 	}
@@ -84,6 +124,14 @@ public class PesquisaSelecaoControl {
 
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
+	}
+
+	public Pessoa getCandidatoConsulta() {
+		return candidatoConsulta;
+	}
+
+	public void setCandidatoConsulta(Pessoa candidatoConsulta) {
+		this.candidatoConsulta = candidatoConsulta;
 	}
 
 	public SelecaoCandidato getSelecaoCandidato() {

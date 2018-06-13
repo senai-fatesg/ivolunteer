1efb96d5323105811b48e0016d044c610c82ba50
guilherme.freitas <guilherme.freitas@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-06-13 23:05:35 +0000

    Correção de tarefa #644 e implementação da tarefa #707

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/GradeHorario.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/GradeHorario.java
@@ -1,5 +1,6 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -7,12 +8,15 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-public class GradeHorario {
+public class GradeHorario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(generator = "gradehorario_seq", strategy = GenerationType.SEQUENCE)
@@ -27,6 +31,25 @@ public class GradeHorario {
 
 	@Temporal(TemporalType.TIME)
 	private Date horarioSaida;
+	
+	@ManyToOne
+	private Funcionario funcionario;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
 
 	public Date getTotal() {
 		return null;
@@ -60,8 +83,7 @@ public class GradeHorario {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((diaSemana == null) ? 0 : diaSemana.hashCode());
+		result = prime * result + ((diaSemana == null) ? 0 : diaSemana.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}

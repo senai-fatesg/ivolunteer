ee06a6afce36101f31f6b00eca635c52e856b495
jose.julio <jose.julio@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-06-14 21:02:06 +0000

    Melhorias no candidato
    * adicionar endereço para candidato ou responsável.
    * exclusão de endereço para candidato ou responsável.

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/CandidatoControl.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/CandidatoControl.java
@@ -34,33 +34,32 @@ import br.com.ambientinformatica.util.UtilLog;
 @Controller("CandidatoControl")
 @Scope("conversation")
 public class CandidatoControl {
-	
+
 	private Pessoa candidato = new Pessoa();
 	private Responsavel responsavel;
 	private Pessoa filtro = new Pessoa();
-	
-	//instancia de candidato carregada para realização de consulta
+
+	// instancia de candidato carregada para realização de consulta
 	private Pessoa candidatoConsulta = new Pessoa();
-	
-	//lista utilizada na consulta do candidato
+
+	// lista utilizada na consulta do candidato
 	private List<Pessoa> listaCandidato = new ArrayList<Pessoa>();
-	
-	//listas e utilizada para a apresentação na grid
+
+	// listas e utilizada para a apresentação na grid
 	private List<Responsavel> listaResponsavel = new ArrayList<Responsavel>();
-	private List<Endereco> listaEndereco = new ArrayList<Endereco>();
 	private List<Telefone> listaTelefone = new ArrayList<Telefone>();
-	
-	//objetos utilizados para tratamento das listas
+
+	// objetos utilizados para tratamento das listas
 	private Telefone telefone = new Telefone();
 	private Endereco endereco = new Endereco();
 	private Cidade cidade = new Cidade();
-	
-	//Atributo utilziado para tratamento de renda total do responsavel
+
+	// Atributo utilziado para tratamento de renda total do responsavel
 	private BigDecimal totalRenda = BigDecimal.ZERO;
-	
+
 	@Autowired
 	private PessoaDao pessoaDao;
-	
+
 	@PostConstruct
 	public void init() {
 		listarCandidatoPorNome(null);
@@ -68,22 +67,22 @@ public class CandidatoControl {
 		responsavel = new Responsavel();
 	}
 
-	public void listarCandidatoPorNome(ActionEvent evt){
+	public void listarCandidatoPorNome(ActionEvent evt) {
 		try {
 			listaCandidato = pessoaDao.listaCandidatoPorNome(candidatoConsulta.getNomePessoa());
 		} catch (Exception e) {
-		   UtilFaces.addMensagemFaces(e);
+			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	public void confirmar(ActionEvent evt) {
 		try {
-		//	preenchaInformacoesDefaultCandidato(candidato);
+			// preenchaInformacoesDefaultCandidato(candidato);
 			candidato.setEnumTipoPessoa(EnumTipoPessoa.CANDIDATO);
 			validarCandidato(candidato);
 			pessoaDao.alterar(candidato);
 			candidato = new Pessoa();
-			
+
 			UtilFaces.addMensagemFaces("Informações salvas com sucesso!");
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e.getMessage());
@@ -91,26 +90,28 @@ public class CandidatoControl {
 		}
 	}
 
-	/**Preenche os dados do candidato automaticamente.
-	 * Obs: As informações preenchidas não afetam a integridade dos dados **/
-	
-	private void preenchaInformacoesDefaultCandidato(Pessoa objeto) {
-	   
-	   objeto.setEnumEstadoCivil(EnumEstadoCivil.SOLTEIRO);
-	   objeto.setEnumPrioridade(EnumPrioridade.BAIXA);
-	   objeto.setProfissao("NULL");
-	   objeto.setRg("NULL");
-	   objeto.setCpf("NULL");
-   }
+	/**
+	 * Preenche os dados do candidato automaticamente. Obs: As informações
+	 * preenchidas não afetam a integridade dos dados
+	 **/
 
-	//Adicionar o telefone do responsavel
+	private void preenchaInformacoesDefaultCandidato(Pessoa objeto) {
+
+		objeto.setEnumEstadoCivil(EnumEstadoCivil.SOLTEIRO);
+		objeto.setEnumPrioridade(EnumPrioridade.BAIXA);
+		objeto.setProfissao("NULL");
+		objeto.setRg("NULL");
+		objeto.setCpf("NULL");
+	}
+
+	// Adicionar o telefone do responsavel
 	public void adicionarTelefone(ActionEvent evt) {
 		try {
-			if(EhTelefoneConsistente()){
+			if (EhTelefoneConsistente()) {
 				responsavel.addTelefone(telefone);
 				telefone = new Telefone();
 				listaTelefone = responsavel.getListaTelefone();
-			}else{
+			} else {
 				UtilFaces.addMensagemFaces("Preencha os campos corretamente");
 			}
 		} catch (Exception e) {
@@ -118,62 +119,81 @@ public class CandidatoControl {
 			UtilLog.getLog().error(e);
 		}
 	}
-	
-	//adicionar o endereco do responsavel
-	public void adicionarEndereco(ActionEvent evt) {
-		try{
-			if(EhEnderecoConsistente()){
-				responsavel.addEndereco(endereco);
+
+	// metodo apenas para chamar o adicionarEndereco passando o candidato
+	public void adicionarEnderecoCandidato(ActionEvent evt) {
+		adicionarEndereco(candidato);
+	}
+
+	// metodo apenas para chamar o adicionarEndereco passando o responsavel
+	public void adicionarEnderecoResponsavel(ActionEvent evt) {
+		adicionarEndereco(responsavel);
+	}
+
+	// adicionar um endereco para o candidato ou responsavel
+	public void adicionarEndereco(Pessoa pessoa) {
+		try {
+			if (EhEnderecoConsistente()) {
+				pessoa.addEndereco(endereco);
 				endereco = new Endereco();
-				listaEndereco = responsavel.getListaEndereco();
-			}else{
+			} else {
 				UtilFaces.addMensagemFaces("Preencha os campos corretamente");
 			}
-		}catch(Exception erro){
+		} catch (Exception erro) {
 			UtilFaces.addMensagemFaces("Ocorreu uma falha ao tentar incluir o endereço na lista");
 			UtilLog.getLog().error(erro);
 		}
 	}
-	
-	/**Calcula a renda do responsavel, a renda e calculada com base na renda + rendaExtra**/
-	public BigDecimal getTotalRenda(){
+
+	/**
+	 * Calcula a renda do responsavel, a renda e calculada com base na renda +
+	 * rendaExtra
+	 **/
+	public BigDecimal getTotalRenda() {
 		return responsavel.calcularRenda();
 	}
-	
-	//Adionar as responsaveis ao candidato
+
+	// Adionar as responsaveis ao candidato
 	public void adicionarResponsavel(ActionEvent evt) {
-		try{
-			if(EhResponsavelConsistente()){
+		try {
+			if (EhResponsavelConsistente()) {
 				preenchaInformcoesDefaultResponsavel(responsavel);
 				responsavel.setEnumTipoPessoa(EnumTipoPessoa.RESPONSAVEL);
 				candidato.addResponsavel(responsavel);
 				listaResponsavel = candidato.getListaResponsavel();
-				//responsavel = new Responsavel();
-			}else{
+				responsavel = new Responsavel();
+			} else {
 				UtilFaces.addMensagemFaces("Preencha os campos corretamente");
 			}
-		}catch(Exception erro){
+		} catch (Exception erro) {
 			UtilFaces.addMensagemFaces("Ocorreu uma falha ao tentar incluir o Responsável na lista.");
 			UtilLog.getLog().error(erro);
 		}
 	}
-	
-	/**Preenche as informções do responsavel, as informções preenchidas não afeta a integridade dos dados**/
-	private void preenchaInformcoesDefaultResponsavel(Responsavel objeto) {
-	   objeto.setCertidaoNascimento("NULL");
-   }
 
-	//remove o endereço do responsavel
-	public void removerEndereco(Endereco endereco) {
+	/**
+	 * Preenche as informções do responsavel, as informções preenchidas não afeta a
+	 * integridade dos dados
+	 **/
+	private void preenchaInformcoesDefaultResponsavel(Responsavel objeto) {
+		objeto.setCertidaoNascimento("NULL");
+	}
+
+	// remove um endereço do responsavel ou candidato
+	public void removerEndereco(Endereco endereco, String candidatoOuResponsavel) {
 		try {
-			responsavel.removerEndereco(endereco);
+			if (candidatoOuResponsavel.equals("Responsavel")) {
+				responsavel.removerEndereco(endereco);
+			} else {
+				candidato.removerEndereco(endereco);
+			}
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces("Ocorreu uma falha ao tentar excluir o endereço da lista");
 			UtilLog.getLog().error(e);
 		}
 	}
-	
-	//remove o telefone do responsavel
+
+	// remove o telefone do responsavel
 	public void removerTelefone(Telefone telefone) {
 		try {
 			responsavel.removerTelefone(telefone);
@@ -182,8 +202,8 @@ public class CandidatoControl {
 			UtilLog.getLog().error(e);
 		}
 	}
-	
-	//remove o responsavel do candidato
+
+	// remove o responsavel do candidato
 	public void removerResponsavel(Responsavel responsavel) {
 		try {
 			candidato.removerResponsavel(responsavel);
@@ -192,30 +212,32 @@ public class CandidatoControl {
 			UtilLog.getLog().error(e);
 		}
 	}
-	
-	/**Remove o objeto candidato, onde a exclusão e realizada com o cascade e 
-	 * é utilizado o metodo de excluir por "id". **/
-	public void removerCandidato(Pessoa candidato){
-		try{
-			if(listaCandidato.contains(candidato)){
+
+	/**
+	 * Remove o objeto candidato, onde a exclusão e realizada com o cascade e é
+	 * utilizado o metodo de excluir por "id".
+	 **/
+	public void removerCandidato(Pessoa candidato) {
+		try {
+			if (listaCandidato.contains(candidato)) {
 				this.pessoaDao.excluirPorId(candidato.getId());
 				listaCandidato = pessoaDao.listar();
 				UtilFaces.addMensagemFaces("Candidato excluido com sucesso!");
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			UtilFaces.addMensagemFaces("Ocorreu uma falha ao tentar excluir o candidato da base de dados.");
 			UtilLog.getLog().error(e);
 		}
 	}
-	
-	public void alterarCandidato(Pessoa candidato){
-		try{
+
+	public void alterarCandidato(Pessoa candidato) {
+		try {
 			listaCandidato.contains(candidato);
-		}catch(Exception e){
+		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	public void excluir(Pessoa pessoa) {
 		try {
 			pessoaDao.excluirPorId(pessoa.getId());
@@ -224,19 +246,19 @@ public class CandidatoControl {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
-	//carrega o objeto do candidato completo
-	public void carregaPessoaAlteracao(Pessoa pessoa) throws PersistenciaException{
+
+	// carrega o objeto do candidato completo
+	public void carregaPessoaAlteracao(Pessoa pessoa) throws PersistenciaException {
 		this.candidato = pessoaDao.consultarPessoaCompleta(pessoa);
 		listaResponsavel = candidato.getListaResponsavel();
 	}
-	
+
 	// Aplica Filtro
 	public void aplicarFiltro(ActionEvent evt) {
 		try {
 			if (candidatoConsulta.getNomePessoa() == null || candidatoConsulta.getNomePessoa().isEmpty()) {
 				listaCandidato = pessoaDao.listaCandidato();
-			} else {				
+			} else {
 				listaCandidato = pessoaDao.listaCandidatoPorNome(this.candidato.getNomePessoa());
 			}
 		} catch (Exception e) {
@@ -244,15 +266,15 @@ public class CandidatoControl {
 		}
 
 	}
-	
+
 	public List<SelectItem> getCompleteEnumFiliacao() {
 		return UtilFaces.getListEnum(EnumFiliacao.values());
 	}
-	
+
 	public List<SelectItem> getCompleteEnumTipoCasa() {
 		return UtilFaces.getListEnum(EnumTipoCasa.values());
 	}
-	
+
 	public List<SelectItem> getCompleteEnumEstado() {
 		return UtilFaces.getListEnum(EnumEstado.values());
 	}
@@ -268,28 +290,24 @@ public class CandidatoControl {
 	public List<SelectItem> getCompleteEnumTipoTelefone() {
 		return UtilFaces.getListEnum(EnumTipoTelefone.values());
 	}
-	
-	public List<SelectItem> getCompleteEnumTipoMoradia(){
+
+	public List<SelectItem> getCompleteEnumTipoMoradia() {
 		return UtilFaces.getListEnum(EnumTipoCasa.values());
 	}
-	
-	public List<SelectItem> getCompleteEnumEstadoCivil(){
+
+	public List<SelectItem> getCompleteEnumEstadoCivil() {
 		return UtilFaces.getListEnum(EnumEstadoCivil.values());
 	}
-	
+
 	public List<SelectItem> getCompleteEnumPrioridade() {
 		return UtilFaces.getListEnum(EnumPrioridade.values());
 	}
-	
+
 	public List<Telefone> getListaTelefone() {
 		return responsavel.getListaTelefone();
 	}
-	
-	public List<Endereco> getListaEndereco() {
-		return responsavel.getListaEndereco();
-	}
-	
-	public void gerarPdf(){
+
+	public void gerarPdf() {
 		return;
 	}
 
@@ -312,45 +330,42 @@ public class CandidatoControl {
 	public void setTotalRenda(BigDecimal totalRenda) {
 		this.totalRenda = totalRenda;
 	}
-	
-	private boolean EhTelefoneConsistente(){
-		if(telefone.getNomePessoaRecado() != null && !telefone.getNomePessoaRecado().isEmpty() &&
-				telefone.getNumeroTelefone() != null && !telefone.getNumeroTelefone().isEmpty()){
-		    return true;
-		}else{
-		    return false;
+
+	private boolean EhTelefoneConsistente() {
+		if (telefone.getNomePessoaRecado() != null && !telefone.getNomePessoaRecado().isEmpty()
+				&& telefone.getNumeroTelefone() != null && !telefone.getNumeroTelefone().isEmpty()) {
+			return true;
+		} else {
+			return false;
 		}
 	}
-	
-	private boolean EhEnderecoConsistente(){
-		if(!endereco.getRuaOuAvenida().isEmpty() && !endereco.getBairro().isEmpty() &&
-				!endereco.getQuadra().isEmpty() && !endereco.getLote().isEmpty()){
-		    return true;
-		}else{
-		    return false;
+
+	private boolean EhEnderecoConsistente() {
+		if (!endereco.getRuaOuAvenida().isEmpty() && !endereco.getBairro().isEmpty() && !endereco.getQuadra().isEmpty()
+				&& !endereco.getLote().isEmpty()) {
+			return true;
+		} else {
+			return false;
 		}
 	}
-	
-	
-	
-public void validarCandidato(Pessoa candidato) throws Exception{
-//		
-//		if (candidato.getNomePessoa().isEmpty()){
-//			throw new Exception("Por favor, informe o nome do candidato");
-//		}
-//		
-//		if (candidato.getNaturalidade().isEmpty()){
-//			throw new Exception("Por favor, informe a naturalidade do candidato");
-//		}
+
+	public void validarCandidato(Pessoa candidato) throws Exception {
 		
+		 if (candidato.getNomePessoa().isEmpty()){
+		    throw new Exception("Por favor, informe o nome do candidato");
+		 }
+		
+		 if (candidato.getNaturalidade().isEmpty()){
+		    throw new Exception("Por favor, informe a naturalidade do candidato");
+		 }
 	}
-	
-	private boolean EhResponsavelConsistente(){
-		if(responsavel.getNomePessoa()!= null && !responsavel.getNomePessoa().isEmpty() &&
-				responsavel.getCpf()!=null && !responsavel.getCpf().isEmpty()){
-		    return true;
-		}else{
-		    return false;
+
+	private boolean EhResponsavelConsistente() {
+		if (responsavel.getNomePessoa() != null && !responsavel.getNomePessoa().isEmpty()
+				&& responsavel.getCpf() != null && !responsavel.getCpf().isEmpty()) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
@@ -377,7 +392,7 @@ public void validarCandidato(Pessoa candidato) throws Exception{
 	public List<Responsavel> getListaResponsavel() {
 		return listaResponsavel;
 	}
-	
+
 	public Pessoa getCandidatoConsulta() {
 		return candidatoConsulta;
 	}
@@ -390,12 +405,12 @@ public void validarCandidato(Pessoa candidato) throws Exception{
 		this.responsavel = responsavel;
 	}
 
-    public Cidade getCidade() {
-        return cidade;
-    }
+	public Cidade getCidade() {
+		return cidade;
+	}
 
-    public void setCidade(Cidade cidade) {
-        this.cidade = cidade;
-    }
-	
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
 }

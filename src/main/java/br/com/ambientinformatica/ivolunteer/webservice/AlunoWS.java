5df72ed73bbd2308ac2a6e607d6a3ab40f97dbe4
pedro.furtado <pedro.furtado@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-06-17 00:34:18 +0000

    ws alunos

--- src/main/java/br/com/ambientinformatica/ivolunteer/webservice/AlunoWS.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/webservice/AlunoWS.java
@@ -1,14 +1,22 @@
 package br.com.ambientinformatica.ivolunteer.webservice;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import br.com.ambientinformatica.ivolunteer.entidade.Aluno;
+import br.com.ambientinformatica.ivolunteer.persistencia.AlunoDao;
 
 @Path("/json/aluno")
 public class AlunoWS {
+	
+	@Autowired
+	public AlunoDao alunoDao;
 	
 	@GET
 	@Path("/get")
@@ -20,6 +28,14 @@ public class AlunoWS {
 
 		return aluno;
 
+	}
+	
+	@GET
+	@Path("/todos")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Aluno> getAlunosJSON(){
+		
+		return alunoDao.listarTodos();
 	}
 
 }

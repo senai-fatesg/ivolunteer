e7f6a4c49219243e2b7b43452052de565f86a778
eduardo.santos <eduardo.santos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-06-22 18:41:40 +0000

    Cadastro de Turma

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/EnumTurno.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/EnumTurno.java
@@ -6,27 +6,34 @@ import br.com.ambientinformatica.util.IEnum;
 
 public enum EnumTurno implements IEnum {
 
-	MATUTINO("1","Matutino"), 
-	VESPERTINO("2","Verpertino"), 
-	NOTURNO("3","Noturno");
+	MATUTINO(1, "Matutino"), VESPERTINO(2, "Verpertino"), NOTURNO(3, "Noturno");
 
-	EnumTurno(String descricao, String codigo) {
-		this.descricao = descricao;
+	EnumTurno(Integer codigo, String descricao) {
 		this.codigo = codigo;
+		this.descricao = descricao;
 	}
-	
+
+	private Integer codigo;
 	private String descricao;
-	private String codigo;
 
 	public String getDescricao() {
 		return descricao;
 	}
 
-	public String getCodigo() {
+	public Integer getCodigo() {
 		return codigo;
 	}
-	
+
+	public static EnumTurno getById(int codigo) {
+		for (EnumTurno turno : EnumTurno.values()) {
+			if (codigo == turno.getCodigo()) {
+				return turno;
+			}
+		}
+		return null;
+	}
+
 	public static EnumSet<EnumTurno> getListaTurnos() {
-        return EnumSet.of(MATUTINO, VESPERTINO, NOTURNO);
-    }
+		return EnumSet.of(MATUTINO, VESPERTINO, NOTURNO);
+	}
 }

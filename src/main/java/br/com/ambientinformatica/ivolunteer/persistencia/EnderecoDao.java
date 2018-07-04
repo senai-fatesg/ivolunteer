0d72fb730c61e6ac35f40da2a3a7866df331d5f9
rubens.santos <rubens.santos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-07-04 19:17:45 +0000

    Criação do atributo isAtivo para desativar o endereço além do método da camada da persistência para inativar o endereço.

--- src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/EnderecoDao.java
+++ src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/EnderecoDao.java
@@ -5,6 +5,8 @@ import br.com.ambientinformatica.jpa.persistencia.Persistencia;
 
 public interface EnderecoDao extends Persistencia<Endereco>{
 
+	public Endereco desativaEndereco(Endereco endereco);
+	
 }
 
 

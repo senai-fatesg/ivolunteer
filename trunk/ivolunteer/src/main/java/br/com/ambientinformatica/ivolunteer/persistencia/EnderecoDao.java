4583a119fb38902c7fef7fae1f49a1be02f22f88
rubens.santos <rubens.santos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-07-26 02:14:18 +0000

    Implementado adição e edição de responsável de candidato. Mensagens de atualização e adição de responsável. Solucionado problemas das linhas vazias nas listas.

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/EnderecoDao.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/persistencia/EnderecoDao.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import br.com.ambientinformatica.ivolunteer.entidade.Candidato;
 import br.com.ambientinformatica.ivolunteer.entidade.Endereco;
+import br.com.ambientinformatica.ivolunteer.entidade.Responsavel;
 import br.com.ambientinformatica.jpa.persistencia.Persistencia;
 
 public interface EnderecoDao extends Persistencia<Endereco>{
 
 	public void desativaEndereco(Endereco endereco);
 	public List<Endereco> buscaTodosEnderecosPorCandidato(Candidato candidato);
-	
+	public List<Endereco> buscaTodosEnderecosPorResponsavel(Responsavel responsavel);
 }
 
 

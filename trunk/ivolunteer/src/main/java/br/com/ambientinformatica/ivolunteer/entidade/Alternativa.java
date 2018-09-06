acafa0c6dd4312f415158c3dd18e5abc830c1d23
rubens.santos <rubens.santos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-09-06 02:18:41 +0000

    Atualizações da tela de Avaliação: - Implementado ordenação das questões; - Implementado regra que impede alterar o tipo de uma questão já cadastrada; - Ao excluir uma questão objetiva, é excluído também suas respectivas alternativas; - Ao editar uma avaliação, a ordem da próxima questão é setada automaticamente; - Implementado regra de negócio que verifica e valida a ordenação das questões e que permite, ao editar uma questão, trocar a ordenação de uma questão pela outra, caso a ordenação selecionada já exista para alguma questão;

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Alternativa.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Alternativa.java
@@ -1,5 +1,7 @@
 package br.com.ambientinformatica.ivolunteer.entidade;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;

48e87c1f8fdb7fbf362a5c72c7d8c5fa5f41c947
rubens.santos <rubens.santos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-10-10 02:28:17 +0000

    Release 03-10-2018 - [x] Pequenas correções no formulário de colaboradores: alguns campos agora passam a aceitar somente letras;
    Release 09-10-2018
    - [x] Reorganização dos elementos que compõe o layout da tela de matricula;
    - [x] Adicionado botão de matricular candidatos para alunos;
    - [x] Adicionado método para matricular candidatos para aluno;

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/MatriculaControl.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/MatriculaControl.java
@@ -16,13 +16,16 @@ import com.ibm.icu.text.SimpleDateFormat;
 
 import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
 import br.com.ambientinformatica.ivolunteer.entidade.Aluno;
+import br.com.ambientinformatica.ivolunteer.entidade.Candidato;
 import br.com.ambientinformatica.ivolunteer.entidade.Cidade;
 import br.com.ambientinformatica.ivolunteer.entidade.Endereco;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumEstado;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumSexo;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumTipoPessoa;
 import br.com.ambientinformatica.ivolunteer.entidade.Matricula;
+import br.com.ambientinformatica.ivolunteer.entidade.Pessoa;
 import br.com.ambientinformatica.ivolunteer.persistencia.AlunoDao;
+import br.com.ambientinformatica.ivolunteer.persistencia.CandidatoDao;
 import br.com.ambientinformatica.ivolunteer.persistencia.MatriculaDao;
 import br.com.ambientinformatica.jpa.exception.PersistenciaException;
 import net.sf.jasperreports.data.mondrian.SimpleSQLDataSource;
@@ -32,19 +35,24 @@ import net.sf.jasperreports.data.mondrian.SimpleSQLDataSource;
 public class MatriculaControl {
 
 	private Aluno aluno = new Aluno();
+	private Candidato candidato = new Candidato();
 	private Cidade cidade = new Cidade();
 	private Endereco endereco = new Endereco();
 	private String dataReal;
 	private Matricula matricula = new Matricula();
 	private String certidaoNascimento; 
+	private String pesquisaNomeCandidato;
 
 	@Autowired
 	private AlunoDao alunoDao;
 	@Autowired
 	private MatriculaDao matriculaDao;
+	@Autowired
+	private CandidatoDao candidatoDao;
 
 	private List<Matricula> matriculas = new ArrayList<Matricula>();
 	private List<Aluno> alunos = new ArrayList<Aluno>();
+	private List<Candidato> candidatos = new ArrayList<Candidato>();
 
 	public Aluno getAluno() {
 		return aluno;
@@ -52,6 +60,14 @@ public class MatriculaControl {
 
 	public void setAluno(Aluno aluno) {
 		this.aluno = aluno;
+	}
+
+	public Candidato getCandidato() {
+		return candidato;
+	}
+
+	public void setCandidato(Candidato candidato) {
+		this.candidato = candidato;
 	}
 
 	public Cidade getCidade() {
@@ -94,6 +110,14 @@ public class MatriculaControl {
 		this.matriculaDao = matriculaDao;
 	}
 
+	public CandidatoDao getCandidatoDao() {
+		return candidatoDao;
+	}
+
+	public void setCandidatoDao(CandidatoDao candidatoDao) {
+		this.candidatoDao = candidatoDao;
+	}
+
 	public void setMatriculas(List<Matricula> matriculas) {
 		this.matriculas = matriculas;
 	}
@@ -102,6 +126,7 @@ public class MatriculaControl {
 	public void init() {
 //		listarAlunos(null);
 		aplicarFiltro(null);
+		listarCandidatos();
 	}
 	
 	public List<Aluno> getAlunos() {
@@ -115,6 +140,14 @@ public class MatriculaControl {
 		this.alunos = alunos;
 	}
 	
+	public List<Candidato> getCandidatos() {
+		return candidatos;
+	}
+
+	public void setCandidatos(List<Candidato> candidatos) {
+		this.candidatos = candidatos;
+	}
+
 	public void incluir(ActionEvent evt) {
 		try {
 			if(this.matricula.getId() == null) {
@@ -153,6 +186,21 @@ public class MatriculaControl {
 		} catch (PersistenciaException e) {
 			UtilFaces.addMensagemFaces(e);
 		}
+	}
+	
+	public void carregaCandidatoParaAlunoMatriculado(Candidato candidato) {
+		this.candidato = candidatoDao.consultarCandidatoCompleto(candidato);
+		
+		this.aluno.setNomePessoa(this.candidato.getNomePessoa());
+		this.aluno.setDataNascimento(this.candidato.getDataNascimento());
+		this.aluno.setEnumSexo(this.candidato.getEnumSexo());
+		this.aluno.setNaturalidade(this.candidato.getNaturalidade());
+		this.aluno.setCpf(this.candidato.getCpf());
+		this.aluno.setRg(this.candidato.getRg());
+		this.aluno.setOrgaoExpeditor(this.candidato.getOrgaoExpeditor());
+		this.aluno.getListaEndereco().addAll(this.candidato.getListaEndereco());
+		
+		this.calculaIdadeReal(null);
 	}
 	
 	public void removerEndereco(Endereco endereco) {
@@ -241,11 +289,23 @@ public class MatriculaControl {
 	public void setCertidaoNascimento(String certidaoNascimento) {
 		this.certidaoNascimento = certidaoNascimento;
 	}
+	public String getPesquisaNomeCandidato() {
+		return pesquisaNomeCandidato;
+	}
+
+	public void setPesquisaNomeCandidato(String nomeDoCandidato) {
+		this.pesquisaNomeCandidato = nomeDoCandidato;
+	}
+
 	public void calculaIdadeReal(SelectEvent event){
 		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 		String dataFormatada = formato.format(aluno.getDataNascimento());
 		this.dataReal = aluno.CalcularIdadeReal(dataFormatada);
 		
+	}
+	
+	public void listarCandidatos() {
+		this.candidatos = candidatoDao.listaCandidatosComResponsavel();
 	}
 	
 }
--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Aluno.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/entidade/Aluno.java
@@ -20,7 +20,7 @@ import javax.persistence.TemporalType;
 
 @Entity
 @PrimaryKeyJoinColumn(name="id")
-public class Aluno extends Pessoa{
+public class Aluno extends Pessoa {
 
 
 	@Temporal(TemporalType.DATE)

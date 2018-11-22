dab2ae96138628e0171f3e58ede5eb5ccd1884d1
rubens.santos <rubens.santos@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2018-11-22 23:44:30 +0000

    Atualizações da tela de turma: - Retirado campos antigos de filtros; - Implementado filtros da própria tabela; - Reorganizado campos na tela;

--- trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/TurmaControl.java
+++ trunk/ivolunteer/src/main/java/br/com/ambientinformatica/ivolunteer/controle/TurmaControl.java
@@ -3,15 +3,11 @@ package br.com.ambientinformatica.ivolunteer.controle;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.annotation.PostConstruct;
 import javax.faces.event.ActionEvent;
-import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.model.SelectItem;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +17,9 @@ import org.springframework.stereotype.Controller;
 import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
 import br.com.ambientinformatica.ivolunteer.entidade.Curso;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumCargo;
-import br.com.ambientinformatica.ivolunteer.entidade.EnumFiliacao;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumTipoCurso;
 import br.com.ambientinformatica.ivolunteer.entidade.EnumTurno;
 import br.com.ambientinformatica.ivolunteer.entidade.Funcionario;
-import br.com.ambientinformatica.ivolunteer.entidade.Pessoa;
 import br.com.ambientinformatica.ivolunteer.entidade.Turma;
 import br.com.ambientinformatica.ivolunteer.persistencia.CursoDao;
 import br.com.ambientinformatica.ivolunteer.persistencia.FuncionarioDao;
@@ -40,12 +34,11 @@ public class TurmaControl implements TurmaService {
 	/*** ATRIBUTOS DA PÁGINA ***/
 	private Turma turma = new Turma();
 	private Curso curso = new Curso();
-	private Curso cursoCadastro = new Curso();
 	private Curso cursoSelecionado;
 	private Turma exibeTurmaInfo = new Turma();
 
-	private String statusFiltro;
 	private Turma turmaConsulta = new Turma();
+	private List<Turma> turmasFiltradas = new ArrayList<>();
 	private List<Turma> turmasConsulta = new ArrayList<>();
 	private Funcionario professorSelecionado;
 
@@ -56,7 +49,6 @@ public class TurmaControl implements TurmaService {
 	@Autowired
 	private FuncionarioDao funcionarioDao;
 
-	
 	private List<Curso> autocompleteCursos = new ArrayList<Curso>();
 	private TurmaService turmaService;
 	private List<Turma> turmas = new ArrayList<>();
@@ -75,241 +67,56 @@ public class TurmaControl implements TurmaService {
 		carregaProfessores();
 		listaCursosAtivos();
 	}
-	
-	/*** GETTERS E SETTERS ***/
-	
-	public void listaCursosAtivos() {
-		this.listaCursos = cursoDao.listarCursosAtivos();
-	}
-	
-	public List<Funcionario> getAutocompleteProfessores() {
-		return autocompleteProfessores;
-	}
-
-	public void setAutocompleteProfessores(List<Funcionario> autocompleteProfessores) {
-		this.autocompleteProfessores = autocompleteProfessores;
-	}
-
-	public List<Curso> getAutocompleteCursos() {
-		return autocompleteCursos;
-	}
-
-	public void setAutocompleteCursos(List<Curso> autocompleteCursos) {
-		this.autocompleteCursos = autocompleteCursos;
-	}
-
-	public boolean isRenderizaDatas() {
-		return renderizaDatas;
-	}
-
-	public void setRenderizaDatas(boolean renderizaDatas) {
-		this.renderizaDatas = renderizaDatas;
-	}
-
-	private void carregaCursos() {
-		this.carregaTodosCursos = cursoDao.listar();
-	}
-	
-	public List<Curso> getListaCursos() {
-		return listaCursos;
-	}
-
-	public void setListaCursos(List<Curso> listaCursos) {
-		this.listaCursos = listaCursos;
-	}
-
-	public List<Curso> getCarregaTodosCursos() {
-		return carregaTodosCursos;
-	}
-
-	public void setCarregaTodosCursos(List<Curso> carregaTodosCursos) {
-		this.carregaTodosCursos = carregaTodosCursos;
-	}
-
-	public List<SelectItem> getCompleteEnumTipoCurso() {
-		return UtilFaces.getListEnum(EnumTipoCurso.values());
-	}
-
-	public Curso getCursoSelecionado() {
-		return cursoSelecionado;
-	}
-
-	public void setCursoSelecionado(Curso cursoSelecionado) {
-		this.cursoSelecionado = cursoSelecionado;
-	}
-
-	public Curso getCursoCadastro() {
-		return cursoCadastro;
-	}
-
-	public void setCursoCadastro(Curso cursoCadastro) {
-		this.cursoCadastro = cursoCadastro;
-	}
-
-	public Curso getExibeCursoInfo() {
-		return exibeCursoInfo;
-	}
-
-	public void setExibeCursoInfo(Curso exibeCursoInfo) {
-		this.exibeCursoInfo = exibeCursoInfo;
-	}
-
-	public Turma getTurma() {
-		return turma;
-	}
-
-	public Curso getCurso() {
-		return curso;
-	}
-
-	public void setCurso(Curso curso) {
-		this.curso = curso;
-	}
-
-	public Funcionario getProfessorSelecionado() {
-		return professorSelecionado;
-	}
-
-	public void setProfessorSelecionado(Funcionario professorSelecionado) {
-		this.professorSelecionado = professorSelecionado;
-	}
-
-	public Turma getExibeTurmaInfo() {
-		return exibeTurmaInfo;
-	}
-
-	public void setExibeTurmaInfo(Turma exibeTurmaInfo) {
-		this.exibeTurmaInfo = exibeTurmaInfo;
-	}
-
-	public String getStatusFiltro() {
-		return statusFiltro;
-	}
-
-	public void setStatusFiltro(String statusFiltro) {
-		this.statusFiltro = statusFiltro;
-	}
-
-	public void setTurma(Turma turma) {
-		this.turma = turma;
-	}
-
-	public Turma getTurmaConsulta() {
-		return turmaConsulta;
-	}
-
-	public void setTurmaConsulta(Turma turmaConsulta) {
-		this.turmaConsulta = turmaConsulta;
-	}
-
-	public List<Turma> getTurmasConsulta() {
-		return turmasConsulta;
-	}
-
-	public void setTurmasConsulta(List<Turma> turmasConsulta) {
-		this.turmasConsulta = turmasConsulta;
-	}
-
-	public List<SelectItem> getTurnos() {
-		return UtilFaces.getListEnum(EnumTurno.values());
-	}
-
-	public TurmaDao getTurmaDao() {
-		return turmaDao;
-	}
-
-	public void setTurmaDao(TurmaDao turmaDao) {
-		this.turmaDao = turmaDao;
-	}
-
-	public FuncionarioDao getFuncionarioDao() {
-		return funcionarioDao;
-	}
-
-	public void setFuncionarioDao(FuncionarioDao funcionarioDao) {
-		this.funcionarioDao = funcionarioDao;
-	}
-
-	public TurmaService getTurmaService() {
-		return turmaService;
-	}
-
-	public void setTurmaService(TurmaService turmaService) {
-		this.turmaService = turmaService;
-	}
-
-	public List<Turma> getTurmas() {
-		return turmas;
-	}
-
-	public void setTurmas(List<Turma> turmas) {
-		this.turmas = turmas;
-	}
-
-	public List<Funcionario> getProfessores() {
-		return professores;
-	}
-
-	public void setProfessores(List<Funcionario> professores) {
-		this.professores = professores;
-	}
 
 	/*** AÇÕES DA PÁGINA ***/
-	
+
 	public List<Curso> buscaCursos(String nome) {
 		this.autocompleteCursos = cursoDao.buscaCursoPorNome(nome);
 		return this.autocompleteCursos;
 	}
-	
+
 	public List<Funcionario> buscaProfessores(String nome) {
 		this.autocompleteProfessores = funcionarioDao.buscaEducadorPorNome(nome);
 		return this.autocompleteProfessores;
 	}
-	
+
 	public void inativarCurso(Curso curso) {
 		Curso c = cursoDao.consultar(curso.getId());
 		c.inativarCurso();
 		cursoDao.alterar(c);
 		this.listaCursos = cursoDao.listarCursosAtivos();
 	}
-	
-	public void editaCurso(Curso curso) {
-		this.cursoCadastro = cursoDao.consultar(curso.getId());
-	}
-	
+
 	public void exibeInfoDoCurso(Curso curso) {
 		this.exibeCursoInfo = curso;
 	}
-	
-	public void selecionaCurso(){
-		//curso = cursoDao.buscaCursoComListaDeTurmasAtivas(this.cursoSelecionado);
-		//this.turma.AdicionarCurso(curso);
-		System.out.println("ENTROU!!!!!");
+
+	public void selecionaCurso() {
 		if (this.turma.getCurso().getDuracao().equals(EnumTipoCurso.DURACAO_INDETERMINADA)) {
 			this.renderizaDatas = false;
 		} else {
 			this.renderizaDatas = true;
 		}
 	}
-	
+
 	public List<Curso> buscaCursosPorNome(String nome) {
 		return this.cursoDao.buscaCursoPorNome(nome);
 	}
-	
+
 	public List<Funcionario> buscaProfessoresPorNome(String nome) {
 		return this.funcionarioDao.buscaEducadorPorNome(nome);
 	}
-	
+
 	public void selecionaProfessor() {
 		this.turma.setProfessor(professorSelecionado);
 	}
-	
+
 	public void cadastrarTurma() {
 		try {
 			this.curso.adicionarTurma(this.turma);
 			this.cursoDao.alterar(this.curso);
-			//validarTurma(this.turma);
-			//turmaDao.incluir(this.turma);
+			// validarTurma(this.turma);
+			// turmaDao.incluir(this.turma);
 			listar();
 			this.curso = new Curso();
 			this.turma = new Turma();
@@ -323,7 +130,7 @@ public class TurmaControl implements TurmaService {
 
 	public void salvarAlteracoesTurma() {
 		try {
-			//validarTurma(this.turma);
+			// validarTurma(this.turma);
 			turmaDao.alterar(this.turma);
 			listar();
 			this.curso = new Curso();
@@ -335,7 +142,7 @@ public class TurmaControl implements TurmaService {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	private void validarTurma(Turma turma) throws Exception {
 		if (turma.getCodigo().isEmpty()) {
 			throw new Exception("É obrigatório informar o Código");
@@ -360,7 +167,7 @@ public class TurmaControl implements TurmaService {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	public void concluirTurma(Turma turma) {
 		try {
 			Turma tm = turmaDao.consultar(turma.getId());
@@ -372,7 +179,7 @@ public class TurmaControl implements TurmaService {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	public void exibeInfoTurma(Turma turma) {
 		this.exibeTurmaInfo = turma;
 	}
@@ -389,36 +196,13 @@ public class TurmaControl implements TurmaService {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void carregaTurmaAlteracao(Turma turma) {
 		this.turma = turmaDao.consultar(turma.getId());
 	}
 
-	public void aplicarFiltro(ActionEvent evt) {
-		try {
-			if (!statusFiltro.isEmpty() && this.turmaConsulta.getId().equals(0)) {
-				turmasConsulta = turmaDao.listarPorStatus(this.statusFiltro);
-			} else if(statusFiltro.isEmpty() && this.turmaConsulta.getId().equals(0)) {
-				turmasConsulta = turmaDao.listar();
-			} else if(statusFiltro.isEmpty() && !this.turmaConsulta.getId().equals(0)) {
-				Turma resultado = turmaDao.consultarPorId(turmaConsulta); 
-				turmasConsulta.clear();
-				turmasConsulta.add(resultado);
-			} else if(!statusFiltro.isEmpty() && !this.turmaConsulta.getId().equals(0)) {
-				Turma resultado = turmaDao.consultarPorIdStatus(turmaConsulta, statusFiltro); 
-				System.out.println("RESULTADO NULO? " + (resultado.equals(null)));
-				if(resultado.getId().equals(0)) {
-					turmasConsulta.clear();
-					return;
-				} else {
-					turmasConsulta.clear();
-					turmasConsulta.add(resultado);
-				}
-			}
-			
-		} catch (Exception e) {
-			UtilFaces.addMensagemFaces(e);
-		}
+	private void carregaCursos() {
+		this.carregaTodosCursos = cursoDao.listar();
 	}
 
 	/*** MÉTODOS DE APOIO ***/
@@ -498,6 +282,172 @@ public class TurmaControl implements TurmaService {
 		}
 
 		return horariosFinal;
+	}
+
+	/*** GETTERS E SETTERS ***/
+
+	public void listaCursosAtivos() {
+		this.listaCursos = cursoDao.listarCursosAtivos();
+	}
+
+	public List<Turma> getTurmasFiltradas() {
+		return turmasFiltradas;
+	}
+
+	public void setTurmasFiltradas(List<Turma> turmasFiltradas) {
+		this.turmasFiltradas = turmasFiltradas;
+	}
+
+	public List<Funcionario> getAutocompleteProfessores() {
+		return autocompleteProfessores;
+	}
+
+	public void setAutocompleteProfessores(List<Funcionario> autocompleteProfessores) {
+		this.autocompleteProfessores = autocompleteProfessores;
+	}
+
+	public List<Curso> getAutocompleteCursos() {
+		return autocompleteCursos;
+	}
+
+	public void setAutocompleteCursos(List<Curso> autocompleteCursos) {
+		this.autocompleteCursos = autocompleteCursos;
+	}
+
+	public boolean isRenderizaDatas() {
+		return renderizaDatas;
+	}
+
+	public void setRenderizaDatas(boolean renderizaDatas) {
+		this.renderizaDatas = renderizaDatas;
+	}
+
+	public List<Curso> getListaCursos() {
+		return listaCursos;
+	}
+
+	public void setListaCursos(List<Curso> listaCursos) {
+		this.listaCursos = listaCursos;
+	}
+
+	public List<Curso> getCarregaTodosCursos() {
+		return carregaTodosCursos;
+	}
+
+	public void setCarregaTodosCursos(List<Curso> carregaTodosCursos) {
+		this.carregaTodosCursos = carregaTodosCursos;
+	}
+
+	public List<SelectItem> getCompleteEnumTipoCurso() {
+		return UtilFaces.getListEnum(EnumTipoCurso.values());
+	}
+
+	public Curso getCursoSelecionado() {
+		return cursoSelecionado;
+	}
+
+	public void setCursoSelecionado(Curso cursoSelecionado) {
+		this.cursoSelecionado = cursoSelecionado;
+	}
+
+	public Curso getExibeCursoInfo() {
+		return exibeCursoInfo;
+	}
+
+	public void setExibeCursoInfo(Curso exibeCursoInfo) {
+		this.exibeCursoInfo = exibeCursoInfo;
+	}
+
+	public Turma getTurma() {
+		return turma;
+	}
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
+	public Funcionario getProfessorSelecionado() {
+		return professorSelecionado;
+	}
+
+	public void setProfessorSelecionado(Funcionario professorSelecionado) {
+		this.professorSelecionado = professorSelecionado;
+	}
+
+	public Turma getExibeTurmaInfo() {
+		return exibeTurmaInfo;
+	}
+
+	public void setExibeTurmaInfo(Turma exibeTurmaInfo) {
+		this.exibeTurmaInfo = exibeTurmaInfo;
+	}
+
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+
+	public Turma getTurmaConsulta() {
+		return turmaConsulta;
+	}
+
+	public void setTurmaConsulta(Turma turmaConsulta) {
+		this.turmaConsulta = turmaConsulta;
+	}
+
+	public List<Turma> getTurmasConsulta() {
+		return turmasConsulta;
+	}
+
+	public void setTurmasConsulta(List<Turma> turmasConsulta) {
+		this.turmasConsulta = turmasConsulta;
+	}
+
+	public List<SelectItem> getTurnos() {
+		return UtilFaces.getListEnum(EnumTurno.values());
+	}
+
+	public TurmaDao getTurmaDao() {
+		return turmaDao;
+	}
+
+	public void setTurmaDao(TurmaDao turmaDao) {
+		this.turmaDao = turmaDao;
+	}
+
+	public FuncionarioDao getFuncionarioDao() {
+		return funcionarioDao;
+	}
+
+	public void setFuncionarioDao(FuncionarioDao funcionarioDao) {
+		this.funcionarioDao = funcionarioDao;
+	}
+
+	public TurmaService getTurmaService() {
+		return turmaService;
+	}
+
+	public void setTurmaService(TurmaService turmaService) {
+		this.turmaService = turmaService;
+	}
+
+	public List<Turma> getTurmas() {
+		return turmas;
+	}
+
+	public void setTurmas(List<Turma> turmas) {
+		this.turmas = turmas;
+	}
+
+	public List<Funcionario> getProfessores() {
+		return professores;
+	}
+
+	public void setProfessores(List<Funcionario> professores) {
+		this.professores = professores;
 	}
 
 }
